290c2b44c67c46984d0fe73a03953197f2a1e718
Charlie <wl00606352@gmail.com>
2021-08-05 12:26:21 +0800

    feat: filter sticky top

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,10 +4,12 @@ import classes from "./Filter.module.css"
 const Filter = () => {
   return (
     <div className={classes.contain}>
-      <h4 className={classes.title}>所有餐廳門市</h4>
-      <FilterForm types="radio" />
-      <FilterForm types="priceRange" />
-      <FilterForm types="dietLimit" />
+      <div className={classes.stick}>
+        <h4 className={classes.title}>所有餐廳門市</h4>
+        <FilterForm types="radio" />
+        <FilterForm types="priceRange" />
+        <FilterForm types="dietLimit" />
+      </div>
     </div>
   )
 }
